c3e073d0a7e7025b5a180eec2e5c5551732ff2d5
p-naef <pascal@p-naefs-imac.local>
2008-11-18 11:54:50 +0100

    added tests

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,11 +1,51 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe News do
+describe 'empthy news should not be saved' do
   before(:each) do
     @news = News.new
   end
 
-  it "should be valid" do
-    @news.should be_valid
+  it "should not be saved" do
+    @news.save().should be_false
+  end
+end
+
+describe 'saving a new with all mendatory attributes' do
+  before(:each) do
+    @news = News.new(:headline => 'test', :leadtext => 'test', :start => Date::today())
+  end
+
+  it "should be saved" do
+    @news.save().should be_true
+  end
+end
+
+describe 'saving a news with just headline should not be possible' do
+  before(:each) do
+    @news = News.new(:headline => 'test')
+  end
+
+  it "should not be saved" do
+    @news.save().should be_false
+  end
+end
+
+describe 'saving a news with just leadtext should not be possible' do
+  before(:each) do
+    @news = News.new( :leadtext => 'test')
+  end
+
+  it "should not be saved" do
+    @news.save().should be_false
+  end
+end
+
+describe 'saving a news with just start date should not be possible' do
+  before(:each) do
+    @news = News.new( :start => Date::today())
+  end
+
+  it "should not be saved" do
+    @news.save().should be_false
   end
 end
